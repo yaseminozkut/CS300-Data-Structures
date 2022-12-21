1c3a7c1666171c5056ceef1cd81828457eb62d6d
yaseminozkut <105317121+yaseminozkut@users.noreply.github.com>
2022-12-21 22:29:06 +0300

    Add DynTemplatedStack.h

---
+++ HW1-Random Maze Generator/DynTemplatedStack.h
@@ -0,0 +1,72 @@
+//Code given by Sabancı University
+
+#include "DynTemplatedStack.h"
+
+template <class Object>
+Stack<Object>::Stack( )
+{
+      topOfStack = NULL;
+}
+
+
+template <class Object>
+bool Stack<Object>::isFull( ) const
+{
+       return false;
+}
+
+
+template <class Object>
+bool Stack<Object>::isEmpty( ) const
+{
+      return topOfStack == NULL;
+}
+
+
+template <class Object>
+const Object & Stack<Object>::top() const
+{
+    return topOfStack->element;
+}
+
+
+template <class Object>
+void Stack<Object>::pop()
+{
+    if (isEmpty())
+        return;
+    ListNode *oldTop = topOfStack;
+    topOfStack = topOfStack->next;
+    delete oldTop;
+}
+
+
+template <class Object>
+void Stack<Object>::push(const Object & x)
+{
+    topOfStack = new ListNode( x, topOfStack );
+}
+
+template <class Object>
+Object Stack<Object>::topAndPop()
+{
+     Object topItem = top();
+     pop();
+     return topItem;
+}
+
+
+template <class Object>
+void Stack<Object>::makeEmpty()
+{
+    while (!isEmpty())
+        pop();
+}
+
+
+template <class Object>
+Stack<Object>::~Stack()
+{
+    makeEmpty();
+}
+
