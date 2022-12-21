ea2295ce06cb7a73b9c593f2b9109f02b1b65e13
yaseminozkut <105317121+yaseminozkut@users.noreply.github.com>
2022-12-21 22:30:34 +0300

    Update DynTemplatedStack.h

--- HW1-Random Maze Generator/DynTemplatedStack.h
+++ HW1-Random Maze Generator/DynTemplatedStack.h
@@ -1,72 +1,37 @@
 //Code given by Sabancı University
 
-#include "DynTemplatedStack.h"
-
-template <class Object>
-Stack<Object>::Stack( )
-{
-      topOfStack = NULL;
-}
+#ifndef DynTemplatedStack_h
+#define DynTemplatedStack_h
+#include <iostream>
+using namespace std;
 
 
 template <class Object>
-bool Stack<Object>::isFull( ) const
-{
-       return false;
-}
+class Stack {
+public:
+    Stack( );
+    ~Stack( );
 
+    bool isEmpty() const;
+    bool isFull() const;
+    void makeEmpty();
 
-template <class Object>
-bool Stack<Object>::isEmpty( ) const
-{
-      return topOfStack == NULL;
-}
+    void pop();
+    void push(const Object & x);
+    Object topAndPop();
+    const Object & top() const;
 
+private:
+    struct ListNode
+    {
+      Object element;
+      ListNode *next;
 
-template <class Object>
-const Object & Stack<Object>::top() const
-{
-    return topOfStack->element;
-}
-
-
-template <class Object>
-void Stack<Object>::pop()
-{
-    if (isEmpty())
-        return;
-    ListNode *oldTop = topOfStack;
-    topOfStack = topOfStack->next;
-    delete oldTop;
-}
-
-
-template <class Object>
-void Stack<Object>::push(const Object & x)
-{
-    topOfStack = new ListNode( x, topOfStack );
-}
-
-template <class Object>
-Object Stack<Object>::topAndPop()
-{
-     Object topItem = top();
-     pop();
-     return topItem;
-}
-
-
-template <class Object>
-void Stack<Object>::makeEmpty()
-{
-    while (!isEmpty())
-        pop();
-}
-
-
-template <class Object>
-Stack<Object>::~Stack()
-{
-    makeEmpty();
-}
+      ListNode(const Object & theElement, ListNode * n = NULL)
+          : element(theElement), next(n) {}
+    };
+    
+    ListNode *topOfStack; 
+};
+#endif /* DynTemplatedStack_h */
 
